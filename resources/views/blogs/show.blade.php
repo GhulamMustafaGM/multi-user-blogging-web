c31d7ecf9f7345a42dbbaa558d3a3b2001d822ad
Ghulam Mustafa <mustafaji@gmail.com>
2020-11-14 23:49:06 +0100

    Worked on delete function

--- resources/views/blogs/show.blade.php
+++ resources/views/blogs/show.blade.php
@@ -8,14 +8,15 @@
     <article>
         <div class="jumbotron">
             <h1>{{ $blog->title }}</h1>
-        </div>
-        <h1><a href=" {{ route('blogs.edit', $blog->id) }}">Edit </a> {{ $blog->title }}</h1>
-    <form method="post" action="{{ route('blogs.delete', $blog->id) }}">
-        @csrf
-        {{ method_field('delete') }}
+        
+        <a class="btn btn-primary btn-x pull-left" href=" {{ route('blogs.edit', $blog->id) }}">Edit</a>
+        <form method="post" action="{{ route('blogs.delete', $blog->id) }}">
+            @csrf
+            {{ method_field('delete') }}
 
-        <button type="submit" class="btn btn-danger" >Delete</button>
-    </form>
+            <button type="submit" class="btn btn-danger" >Delete</button>
+            </form>
+        </div>
         <div class="col-md-12">
             <p>{{ $blog->body }}</p>
         </div>
