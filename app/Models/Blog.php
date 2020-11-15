c1664ac3c50702267664c50876b47f1aceddc20c
Ghulam Mustafa <mustafaji@gmail.com>
2020-11-16 00:50:58 +0100

    worked on controllers, middleware

--- app/Models/Blog.php
+++ app/Models/Blog.php
@@ -1,12 +1,20 @@
 <?php
 
-namespace App\Models;
+namespace App;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
-class Blog extends Model
-{
-    use HasFactory;
-    protected $fillable = ['title', 'body'];
+class Blog extends Model {
+	use SoftDeletes;
+	protected $dates = ['deleted_at'];
+	protected $fillable = ['title', 'body', 'featured_image', 'slug', 'meta_title', 'meta_description', 'status'];
+
+	public function category() {
+		return $this->belongsToMany(Category::class);
+	}
+
+	public function user() {
+		return $this->belongsTo(User::class);
+	}
 }
